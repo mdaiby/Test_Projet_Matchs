1e3b35bf2753d2f83ffe668f4ea8473f3584f617
mdaiby <mdiaby@epsi.fr>
2013-03-09 12:21:56 +0100

    Create ClubsTest.cs

---
+++ ClubsTest.cs
@@ -0,0 +1,21 @@
+1 +﻿using System;
+2	+using Microsoft.VisualStudio.TestTools.UnitTesting;
+3	+using Matchs_Lib;
+4	+using System.Collections.Generic;
+5	+
+6	+namespace Matchs_LibTest
+7	+{
+8	+    [TestClass]
+9	+    public class ClubsTest
+10	+    {
+11	+       
+12	+        [TestMethod]
+13	+        public void TestToString()
+14	+        {
+15	+            String nom = "Bordeaux";
+16	+            Clubs C1 = new Clubs(nom);
+17	+            Assert.AreEqual(nom, C1.ToString());
+18	+
+19	+        }
+20	+    }
+21	+}
