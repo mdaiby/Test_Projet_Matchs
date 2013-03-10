1def441cc840bade279f591cda79e63856571a9d
mdaiby <mdiaby@epsi.fr>
2013-03-10 10:26:40 +0100

    Modif FrechLeague1pointSyetem
    Factoring

--- FrenchLeague1PointSystemTest.cs
+++ FrenchLeague1PointSystemTest.cs
@@ -8,30 +8,24 @@ namespace Matchs_LibTest
     [TestClass]
     public class FrenchLeague1PointSystemTest
     {
-        
+        public static FrenchLeague1PointSystem FLPS = (FrenchLeague1PointSystem)FrenchLeague1PointSystem.Instance;
         [TestMethod]
         public void TestInstance()
         {
-            FrenchLeague1PointSystem Pleague = new FrenchLeague1PointSystem();
-            PointSystem PS = FrenchLeague1PointSystem.Instance;
-            Assert.AreEqual(PS, FrenchLeague1PointSystem.Instance);
-           
+            Assert.AreEqual(FLPS, FrenchLeague1PointSystem.Instance);          
         }
         [TestMethod]
         public void TestInitialPoint()
         {
-            FrenchLeague1PointSystem FL = new FrenchLeague1PointSystem();
-            PointSystem.ITotal IT = FL.InitialPoint;
-            Assert.AreEqual(0, FL.InitialPoint.CompareTo(IT));
+            PointSystem.ITotal IT = FLPS.InitialPoint;
+            Assert.AreEqual(0, FLPS.InitialPoint.CompareTo(IT));
         }
 
         [TestMethod]
         public void TestGetPointFromMatch()
-        {
-            PointSystem ip = FrenchLeague1PointSystem.Instance;
-            PointSystem.ITotal IT = ip.GetPointFromMatch(new Matchs_Lib.Matchs(new Clubs("lille"),new Clubs( "Bordeaux"), 2, 1), true);
-            
-            Assert.AreEqual(1, ip.GetPointFromMatch(new Matchs_Lib.Matchs(new Clubs("lille"), new Clubs("Bordeaux"), 1, 1), true).CompareTo(IT));
+        {            
+            PointSystem.ITotal IT = FLPS.GetPointFromMatch(new Matchs_Lib.Matchs(new Clubs("lille"),new Clubs( "Bordeaux"), 3, 0), true);            
+            Assert.AreEqual(3, FLPS.GetPointFromMatch(new Matchs_Lib.Matchs(), true).CompareTo(IT));
         }
 
        }
